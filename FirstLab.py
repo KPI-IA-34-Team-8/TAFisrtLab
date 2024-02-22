9c936b105be0be9ef4f4ae2e0013094b9973d47d
MrGlebTiK <glebtik1@gmail.com>
2024-02-22 19:09:30 +0200

    Add comparison counter
    + change TestResults + change output strings a bit

--- FirstLab.py
+++ FirstLab.py
@@ -4,20 +4,24 @@ import time
 import math
 
 # o is the counter of how many replacements were done in the function.
+# p is the counter of how many comparisons there were done in the function.
 
 def bubble_sort(arr):
     l=len(arr)
     o = 0
+    p = 0
     for i in range(len(arr)-1):
         for j in range(0,l-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
                 o += 1
-    return o
+            p += 1
+    return o, p
 
 def modified_bubble_sort(arr):
     l=len(arr)
     o = 0
+    p = 0
     for i in range(l-1):
         flag = 0
         for j in range(0,l-i-1):
@@ -25,38 +29,46 @@ def modified_bubble_sort(arr):
                 flag=1
                 arr[j],arr[j+1]=arr[j+1],arr[j]  
                 o += 1
+            p += 1
         if flag==0:
             break
-    return o
+        p += 1
+    return o, p
 
 def shell_sort_knuth(arr):
     jump = 0
     l = len(arr)
     o = 0
+    p = 1
     while jump < l:
         jump = jump * 3 + 1
+        p += 1
     jump = round(jump)
+    p += 1
     while jump > 0:
         for i in range(jump, l):
             v = arr[i]
             ii = i
+            p += 2
             while ii > jump - 1 and arr[ii - jump] >= v:
                 arr[ii] = arr[ii - jump]
                 o += 1
                 ii = ii - jump
+                p += 2
             arr[ii] = v
             o += 1
         jump = round((jump - 1)/3)
-    return o
+        p += 1
+    return o, p
 
         
 
-def test_sort(amount: int, alg: callable[[list], int], sorting: int = 1) -> str:
+def test_sort(amount: int, alg: callable, sorting: int = 1) -> str:
     """Tests the sorting alogrithms in the code.
 
     Args:
         amount (int): The length of the array that will be created
-        alg (function): The function that will sort the array in ascending order that will return the amount of replacements that were done during the sorting of the algorithm
+        alg (function): The function that will sort the array in ascending order that will return 2 ints: 1st being o, second being p (look at lines 6-7 for explanation)
         sorting (int, optional): The type of sorting. 1 = in (ascending) order, 2 in opposite (descending) order, 3 = in random order. Defaults to 1.
 
     Returns:
@@ -82,15 +94,16 @@ def test_sort(amount: int, alg: callable[[list], int], sorting: int = 1) -> str:
     print("Unsorted array:")
     # we do this to make sure we can dump it into a txt file
     # otherwise it errors
-    print(str(arr).encode("utf-8"))
+    print(str(arr).encode('utf-8'))
     # get the time in nanoseconds
     # note: time does not change after a nanosecond, it changes after a millisecond
     # so we can't take the array and sort it one time and do time_start - time_end, because it will = 0
     start_time = time.time_ns()
     # times we ran the sorting alogrithms
     ran = 0
-    # the amount of replacements we have done (required in the report)
+    # lines 6-7 for explanation
     o = 0
+    p = 0
     # we do sorting a ton of times, but if it takes more than a second then we stop
     for i in range(1000000):
         # we copy the array so that the original array does not get sorted
@@ -101,8 +114,8 @@ def test_sort(amount: int, alg: callable[[list], int], sorting: int = 1) -> str:
         # if we have done sorting for more than one second
         if time.time_ns() - start_time > 10**9:
             # we sort the original array 
-            # get the amount of replacements we have done in the array that was sorted
-            o = alg(arr)
+            # lines 6-7 for explanations
+            o, p = alg(arr)
             ran += 1
             break
     # https://gist.github.com/dakaugu/d01213ae54f304900f5e918dd07953ad
@@ -116,19 +129,17 @@ def test_sort(amount: int, alg: callable[[list], int], sorting: int = 1) -> str:
     elapsed_time = elapsed_time - math.floor(seconds)
     ms = elapsed_time * 1000
     micros = elapsed_time * 1000 * 1000
-    print("Sorted array:")
-    print(str(arr).encode("utf-8"))
-    print("Amount of replacements in the array:")
-    print(o)
+    rs = f"Amount of replacements that were done: {o}\nAmount of comparisons that were done: {p}\n"
+    rse = f"\nSorted array:\n{str(arr).encode('utf-8')}"
     if hours > 1:
-        return f"Ran the code {ran} times. Time required for one run is: \nApprox. {hours} hours {minutes} minutes {seconds} seconds"
+        return f"{rs}Ran the code {ran} times. Time required for one run is: \nApprox. {hours} hours {minutes} minutes {seconds} seconds{rse}"
     elif minutes > 1:
-        return f"Ran the code {ran} times. Time required for one run is: \nApprox. {minutes} minutes {seconds} seconds"
+        return f"{rs}Ran the code {ran} times. Time required for one run is: \nApprox. {minutes} minutes {seconds} seconds{rse}"
     elif seconds > 1:
-        return f"Ran the code {ran} times. Time required for one run is: \nApprox. {seconds} seconds {ms} ms"
+        return f"{rs}Ran the code {ran} times. Time required for one run is: \nApprox. {seconds} seconds {ms} ms{rse}"
     elif ms > 1:
-        return f"Ran the code {ran} times. Time required for one run is: \nApprox. {ms} ms"
-    return f"Ran the code {ran} times. Time required for one run is: \nApprox. {micros} μs"
+        return f"{rs}Ran the code {ran} times. Time required for one run is: \nApprox. {ms} ms{rse}"
+    return f"{rs}Ran the code {ran} times. Time required for one run is: \nApprox. {micros} μs{rse}"
         
 
 
@@ -141,26 +152,26 @@ if __name__ == "__main__":
     for i in amounts:
         print(f"\n\n\nTesting modified bubble sort with {i} elements:")
         print("\nTest on a sorted algorithm:")
-        print(test_sort(i, modified_bubble_sort, 1).encode("utf-8"))
+        print(test_sort(i, modified_bubble_sort, 1))
         print("\nTest on an \"opposite\" sorted algorithm:")
-        print(test_sort(i, modified_bubble_sort, 2).encode("utf-8"))
+        print(test_sort(i, modified_bubble_sort, 2))
         print("\nTest on an unsorted algorithm:")
-        print(test_sort(i, modified_bubble_sort, 3).encode("utf-8"))
+        print(test_sort(i, modified_bubble_sort, 3))
     print("\n\n\n")
     for i in amounts:
         print(f"\n\n\nTesting bubble sort with {i} elements:")
         print("\nTest on a sorted algorithm:")
-        print(test_sort(i, bubble_sort, 1).encode("utf-8"))
+        print(test_sort(i, bubble_sort, 1))
         print("\nTest on an \"opposite\" sorted algorithm:")
-        print(test_sort(i, bubble_sort, 2).encode("utf-8"))
+        print(test_sort(i, bubble_sort, 2))
         print("\nTest on an unsorted algorithm:")
-        print(test_sort(i, bubble_sort, 3).encode("utf-8"))
+        print(test_sort(i, bubble_sort, 3))
     print("\n\n\n")
     for i in amounts:
         print(f"\n\n\nTesting shell sort (Knuth sequence) with {i} elements:")
         print("\nTest on a sorted algorithm:")
-        print(test_sort(i, shell_sort_knuth, 1).encode("utf-8"))
+        print(test_sort(i, shell_sort_knuth, 1))
         print("\nTest on an \"opposite\" sorted algorithm:")
-        print(test_sort(i, shell_sort_knuth, 2).encode("utf-8"))
+        print(test_sort(i, shell_sort_knuth, 2))
         print("\nTest on an unsorted algorithm:")
-        print(test_sort(i, shell_sort_knuth, 3).encode("utf-8"))
+        print(test_sort(i, shell_sort_knuth, 3))
